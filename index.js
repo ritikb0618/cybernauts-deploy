dac7ad17a62348389ea3ed0775f8d7e94c4e9b5c
ritikb0618 <ritikb0618@gmail.com>
2025-03-14 03:37:02 +0530

    done

--- index.js
+++ index.js
@@ -1,108 +1,262 @@
-require('dotenv').config()
-const express = require("express");
-const path = require("path");
-const userModel=require('./models/user')
-const userModel2=require('./models/message')
-const dbConnection=require('./config/db');
+import { neonCursor } from "https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js";
 
-const app = express();
-
-app.set("views", path.join(__dirname, "public")); 
-
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(express.static("public"))
-
-app.set("view engine", "ejs");
-
-app.get("/", (req, res) => {
-  res.render("index");
+neonCursor({
+  el: document.getElementById("app"),
+  shaderPoints: 9,
+  curvePoints: 20,
+  curveLerp: 1,
+  radius1: 1,
+  radius2: 5,
+  velocityTreshold: 10,
+  sleepRadiusY: 100,
+  sleepRadiusX: 100,
+  sleepTimeCoefX: 0.00025,
+  sleepTimeCoefY: 0.001,
 });
-app.get("/form",(req,res)=>{
-  res.render("form")
-})
 
-app.post("/form", async (req, res) => {
-  try {
-    const { firstName, lastName, phone, email, branch,domain, skills } = req.body;
-    
-    await userModel.create({
-      firstName: firstName,
-      lastName: lastName,
-      phone: phone,
-      email: email,
-      branch: branch,
-      domain:domain,
-      skills: skills
+// ********************prefooter***********************
+var elem = document.querySelector(".ani");
+var chotu = document.querySelector(".chotu");
+
+elem.addEventListener("mouseenter", function () {
+  gsap.to(chotu, {
+    duration: 0.4,
+    scale: 1,
+    opacity: 1,
+    ease: "power2.out",
+  });
+});
+
+elem.addEventListener("mouseleave", function () {
+  gsap.to(chotu, {
+    duration: 0.4,
+    scale: 0,
+    opacity: 0,
+    ease: "power2.inOut",
+  });
+});
+
+elem.addEventListener("mousemove", function (event) {
+  var rect = elem.getBoundingClientRect();
+
+  var x = event.clientX - rect.left;
+  var y = event.clientY - rect.top;
+
+  gsap.to(chotu, {
+    duration: 0.05,
+    x: x,
+    y: y,
+    ease: "power2.out",
+  });
+});
+
+const inputField = document.getElementById("query-input");
+const placeholder = document.getElementById("query-placeholder");
+
+inputField.addEventListener("focus", () => {
+  placeholder.style.top = "-1.5vw";
+  placeholder.style.fontSize = "0.9vw";
+  placeholder.style.color = "#ffffff";
+});
+
+inputField.addEventListener("blur", () => {
+  if (inputField.value === "") {
+    placeholder.style.top = "0.55vw";
+    placeholder.style.fontSize = "1vw";
+    placeholder.style.color = "rgba(255, 255, 255, 0.5)";
+  }
+});
+// ****************************about us**********************************
+
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.from(".abouttext h1 span", {
+    y: 20,
+    duration: 1,
+    stagger: 0.2,
+  });
+
+  let valueDisplays = document.querySelectorAll(".num");
+  let interval = 1000;
+  let observerOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.9,
+  };
+
+  let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        valueDisplays.forEach((valueDisplay) => {
+          let startValue = 0;
+          let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+          let duration = Math.floor(interval / endValue);
+          let counter = setInterval(function () {
+            startValue += 1;
+            valueDisplay.textContent = startValue + "+";
+            if (startValue == endValue) {
+              clearInterval(counter);
+            }
+          }, duration);
+        });
+        observer.unobserve(entry.target); // Stop observing once the animation has played
+      }
     });
-    
-    res.redirect('/')
-  } catch (error) {
-    res.send("app crashed with error: ",error)
-  }
+  }, observerOptions);
+
+  let target = document.querySelector(".about");
+  observer.observe(target);
 });
 
-app.get('/form/admin_login',(req,res)=>{
-  res.render('admin_login')
-})
+// *****************projects***************************//
+// Explore Button
+document
+  .getElementById("Explore_Button")
+  .addEventListener("click", function () {
+    const exploreButton = document.getElementById("Explore_Button");
+    const extraProjects = document.querySelectorAll(
+      "#extra_project1, #extra_project2"
+    );
+    const isShowing = exploreButton.classList.toggle("showing");
 
-app.post('/form/admin_login', async (req, res) => {
-  const { password } = req.body;
+    extraProjects.forEach((project) => {
+      project.style.display = isShowing ? "block" : "none";
+    });
 
-  // Debugging: Log the received and stored passwords
-  // console.log("Received password:", password);
-  // console.log("Stored password:", process.env.ADMIN);
+    const moreIconTemplate = `
+    <span class="button__icon-wrapper">
+      <svg width="10" class="button__icon-svg" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 15">
+        <path fill="currentColor" d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"></path>
+      </svg>
+      <svg class="button__icon-svg button__icon-svg--copy" xmlns="http://www.w3.org/2000/svg" width="10" fill="none" viewBox="0 0 14 15">
+        <path fill="currentColor" d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"></path>
+      </svg>
+    </span>
+  `;
 
-  if (password.trim() !== process.env.ADMIN.trim()) {
-    return res.status(401).json({ error: "Admin ban ja pehle" }); // Return JSON for invalid password
-  }
+    exploreButton.innerHTML = `${moreIconTemplate} ${
+      isShowing ? "Show less" : "Explore All"
+    }`;
+  });
 
-  try {
-    const data = await userModel.find({});
-    res.json(data); // Return data as JSON
-  } catch (e) {
-    res.status(500).json({ error: "Server error" }); // Return JSON for server errors
-  }
+/*----------------------------------------------------------------------------------------------------------------------------- */
+
+/*----------------------------------------------------------------------------------------------------------------------------- */
+/* Animation of Amazing-Crazy-Cool */
+
+function amazingCrazyCool() {
+  // Recursive call for continuous loop
+  amazing();
+  setTimeout(crazy, 550); // Delay next function call by 1 second
+}
+
+function amazing() {
+  document.getElementById("Animation-a-c-c-a").innerHTML = "Amazing";
+}
+
+function crazy() {
+  document.getElementById("Animation-a-c-c-a").innerHTML = "Crazy";
+  setTimeout(cool, 550); // Delay next function call by 1 second
+}
+
+function cool() {
+  document.getElementById("Animation-a-c-c-a").innerHTML = "Cool";
+  setTimeout(awesome, 550); // Delay next function call by 1 second
+}
+
+function awesome() {
+  document.getElementById("Animation-a-c-c-a").innerHTML = "Awesome";
+  setTimeout(amazingCrazyCool, 550); // Call back to start over
+}
+
+// Call the animation function once to initiate the loop
+amazingCrazyCool();
+
+/* Animation of Amazing-Crazy-Cool */
+/*----------------------------------------------------------------------------------------------------------------------------- */
+
+// // Responsive text sizefunction adjustFontSize() {
+const width = window.innerWidth;
+let scaleFactor;
+
+console.log(width);
+
+if (width <= 755) {
+  scaleFactor = "0.24";
+} else if (width <= 782) {
+  scaleFactor = "0.29";
+} else if (width <= 835) {
+  scaleFactor = "0.34";
+} else if (width <= 885) {
+  scaleFactor = "0.39";
+} else if (width <= 875) {
+  scaleFactor = "0.44";
+} else if (width <= 912) {
+  scaleFactor = "0.45";
+} else if (width <= 945) {
+  scaleFactor = "0.49";
+} else if (width <= 982) {
+  scaleFactor = "0.54";
+} else if (width <= 992) {
+  scaleFactor = "0.59";
+} else if (width <= 1149) {
+  scaleFactor = "0.64";
+} else if (width <= 1070) {
+  scaleFactor = "0.69";
+} else if (width <= 1130) {
+  scaleFactor = "0.74";
+} else if (width <= 1200) {
+  scaleFactor = "0.79";
+} else if (width >= 2500) {
+  scaleFactor = "1.49";
+} else {
+  scaleFactor = "1";
+}
+const elements = [
+  ...document.querySelectorAll(".font-size1"),
+  ...document.querySelectorAll(".font-size2"),
+  ...document.querySelectorAll(".font-size3"),
+  ...document.querySelectorAll(".font-size4"),
+  ...document.querySelectorAll(".text"),
+];
+
+elements.forEach((element) => {
+  const baseFontSize = parseFloat(getComputedStyle(element).fontSize);
+  element.style.fontSize = `${baseFontSize * scaleFactor}px`;
 });
 
-app.post('/message',async(req,res)=>{
-  const {email,message}=req.body
+console.log(scaleFactor);
 
-  await userModel2.create({
-    email:email,
-    message:message
-  });
+function adjustImageSize() {
+  const images = document.querySelectorAll(".responsive-image");
+  const width = window.innerWidth;
+  let imageWidth;
 
-  res.redirect('/')
-})
-
-app.get('/list_messages',(req,res)=>{
-  res.render('list_messages')
-})
-
-app.post('/list_messages',async (req,res)=>{
-  const { password } = req.body;
-
-  // Debugging: Log the received and stored passwords
-  // console.log("Received password:", password);
-  // console.log("Stored password:", process.env.ADMIN);
-
-  if (password.trim() !== process.env.MESSAGE.trim()) {
-    return res.status(401).json({ error: "Admin ban ja pehle" }); // Return JSON for invalid password
+  if (width <= 576) {
+    imageWidth = "50%";
+  } else if (width <= 768) {
+    imageWidth = "60%";
+  } else if (width <= 992) {
+    imageWidth = "70%";
+  } else if (width <= 1200) {
+    imageWidth = "80%";
+  } else {
+    imageWidth = "100%";
   }
 
-  try {
-    const data = await userModel2.find({});
-    res.json(data); // Return data as JSON
-  } catch (e) {
-    res.status(500).json({ error: "Server error" }); // Return JSON for server errors
-  }
-})
-
-dbConnection
-  .then(() => {
-    app.listen(3000, () => console.log("Server running on port 3000"));
-  })
-  .catch((err) => {
-    console.error("Database connection failed:", err);
+  images.forEach((image) => {
+    image.style.width = imageWidth;
   });
+}
+
+function adjustSizes() {
+  adjustFontSize();
+  adjustImageSize();
+}
+
+window.addEventListener("load", adjustSizes);
+window.addEventListener("resize", adjustSizes);
+window.addEventListener("reload", adjustSizes);
+
+//--------------------------------------------------------------------------------------------------------------------------------
+
+//Flipping the image of faculty when hovered
--- public/ritik.js
+++
@@ -1,6 +0,0 @@
-const admin=document.querySelector(".admin");
-
-admin.addEventListener('click',function(){
-    window.location.href="/form/admin_login";
-})
-
